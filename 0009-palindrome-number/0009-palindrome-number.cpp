91d009bfd935d065cd23e113764b38c58a6e0543
Tarun Kumar <63495914+doubtcrack@users.noreply.github.com>
2024-06-02 23:06:53 +0530

    Time: 7 ms (71.99%), Space: 9 MB (25.93%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -4,12 +4,27 @@ public:
         // Negative numbers are not palindromes
         if (x < 0) return false;
 
-        long revNum = 0, num = x;
-        while(num!=0){
-            revNum = revNum*10 + num%10;
-            num/=10;
+        /* First Method */
+
+        // long revNum = 0, num = x;
+        // while(num!=0){
+        //     revNum = revNum*10 + num%10;
+        //     num/=10;
+        // }
+        // if(revNum == x) return true;
+        // else return false;
+
+        /* Second Method (String) */
+
+        string str = to_string(x);
+        int i = 0, j=str.length()-1;
+        while(i<j){
+            if(str[i]!=str[j]) return false;
+            else {
+                i++;
+                j--;
+            }
         }
-        if(revNum == x) return true;
-        else return false;
+        return true;
     }
 };
