63757bcecefc636afec927917931ddcbe7516b32
Tarun Kumar <63495914+doubtcrack@users.noreply.github.com>
2024-06-17 22:36:58 +0530

    Time: 0 ms (100%), Space: 29.4 MB (18.45%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
     bool judgeSquareSum(int c) {
-        // vector<int> sq;
-        // for(int i=0;i<=c/2;i++) sq.push_back(i*i);
-        // int n = sq.size();
-        // int i=0, j=n-1;
-        // while(i<j) {
-        //     if(sq[i]+sq[j]==c) return true;
-        //     else if (sq[i]+sq[j]> c) j--;
-        //     else if (sq[i]+sq[j]<c) i++;
-        // }
-
-        for (long a = 0; a * a <= c; ++a) {
-        long b2 = c - a * a;
-        long b = sqrt(b2);
-        if (b * b == b2) {
-            return true;
+// ---------------- Approach 1 ------------------- //
+        vector<long> sq;
+        for(long i=0;i*i<=c;i++) sq.push_back(i*i);
+        int n = sq.size();
+        int i=0, j=n-1;
+        while(i<=j) {
+            if(sq[i]+sq[j]==c) return true;
+            else if (sq[i]+sq[j]> c) j--;
+            else if (sq[i]+sq[j]<c) i++;
         }
-    }
+
+// ---------------- Approach 2 ------------------- //
+
+        // for (long a = 0; a * a <= c; ++a) {
+        // long b2 = c - a * a;
+        // long b = sqrt(b2);
+        //     if (b * b == b2) {
+        //         return true;
+        //     }
+        // }
         return false;
     }
 };
