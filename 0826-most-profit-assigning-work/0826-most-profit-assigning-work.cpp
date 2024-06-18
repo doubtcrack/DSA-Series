7fe90048bd2c8bfddf3a2bd05ecb85ad1128d850
Tarun Kumar <63495914+doubtcrack@users.noreply.github.com>
2024-06-18 23:27:34 +0530

    Time: 38 ms (94.33%), Space: 39.7 MB (89.18%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.cpp
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.cpp
@@ -1,17 +1,48 @@
 class Solution {
 public:
     int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
-        int max_profit=0;
-        for(int i=0;i<worker.size();i++){
-            int max_val = 0;
-            for(int j=0;j<difficulty.size();j++){
-            if(difficulty[j]<=worker[i]){
-                max_val = max(max_val,profit[j]);
-            }
-            }
-            max_profit += max_val;
-            cout<< max_val <<" ";
+
+// -------------- First Approach ------------------- //
+
+        // int max_profit=0;
+        // for(int i=0;i<worker.size();i++){
+        //     int max_val = 0;
+        //     for(int j=0;j<difficulty.size();j++){
+        //     if(difficulty[j]<=worker[i]){
+        //         max_val = max(max_val,profit[j]);
+        //     }
+        //     }
+        //     max_profit += max_val;
+        //     cout<< max_val <<" ";
+        // }
+        // return max_profit;
+
+// -------------- Second Approach ---------------- // 
+// ----- Time Complexity: O(nlogn+mlogm) ------- // 
+
+
+    int n = difficulty.size();
+    int m = worker.size();
+    
+    vector<pair<int, int>> jobs(n);
+    for (int i = 0; i < n; ++i) {
+        jobs[i] = {difficulty[i], profit[i]};
+    }
+
+    // Sort jobs based on difficulty
+    sort(jobs.begin(), jobs.end());
+    sort(worker.begin(), worker.end());
+
+    int maxProfit = 0, jobIndex = 0, bestProfit = 0;
+
+    for (int i = 0; i < m; ++i) {
+        while (jobIndex < n && jobs[jobIndex].first <= worker[i]) {
+            bestProfit = max(bestProfit, jobs[jobIndex].second);
+            ++jobIndex;
         }
-        return max_profit;
+        maxProfit += bestProfit;
+    }
+    
+    return maxProfit;
     }
 };
