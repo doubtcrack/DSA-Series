08b8d7c3146e0561d8a43271300849498715c934
Tarun Kumar <63495914+doubtcrack@users.noreply.github.com>
2024-07-13 15:11:15 +0530

    Time: 199 ms (82.44%), Space: 208.4 MB (63.36%) - LeetHub

---
+++ 2751-robot-collisions/2751-robot-collisions.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+struct Robot {
+    int position;
+    int health;
+    char direction;
+    int index; 
+};
+    vector<int> survivedRobotsHealths(vector<int>& positions, vector<int>& healths, string directions) {
+        int n = positions.size();
+        vector<Robot> robots(n);
+
+        for (int i = 0; i < n; ++i) {
+            robots[i] = {positions[i], healths[i], directions[i], i};
+        }
+
+        // Sort robots by position
+        sort(robots.begin(), robots.end(), [](const Robot &a, const Robot &b) {
+            return a.position < b.position;
+        });
+
+        stack<Robot> st;
+        for (auto &robot : robots) {
+            if (robot.direction == 'R') {
+                st.push(robot);
+            } else { 
+                while (!st.empty() && st.top().direction == 'R') {
+                    Robot top = st.top();
+                    if (top.health < robot.health) {
+                        st.pop();
+                        robot.health -= 1;
+                    } else if (top.health == robot.health) {
+                        st.pop();
+                        robot.health = -1; // robot is also destroyed
+                        break;
+                    } else {
+                        st.top().health -= 1;
+                        robot.health = -1;
+                        break;
+                    }
+                }
+                if (robot.health > 0) {
+                    st.push(robot);
+                }
+            }
+        }
+    
+        // Collect survivors
+        vector<int> result(n, -1);
+        while (!st.empty()) {
+            Robot robot = st.top();
+            st.pop();
+            result[robot.index] = robot.health;
+        }
+        result.erase(remove(result.begin(), result.end(), -1), result.end());
+        return result;
+    }
+};
