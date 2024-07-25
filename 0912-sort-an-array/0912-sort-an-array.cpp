da177e2c7675c5ee86933d0d547f1fd7b3e788e0
Tarun Kumar <63495914+doubtcrack@users.noreply.github.com>
2024-07-25 22:45:17 +0530

    Time: 828 ms (8.09%), Space: 178.4 MB (28.06%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,7 +1,55 @@
 class Solution {
 public:
+
+void merge(vector<int>& nums, int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+    
+    vector<int> leftArr(n1);
+    vector<int> rightArr(n2);
+    
+    for (int i = 0; i < n1; ++i)
+        leftArr[i] = nums[left + i];
+    for (int j = 0; j < n2; ++j)
+        rightArr[j] = nums[mid + 1 + j];
+    
+    int i = 0, j = 0, k = left;
+    while (i < n1 && j < n2) {
+        if (leftArr[i] <= rightArr[j]) {
+            nums[k] = leftArr[i];
+            i++;
+        } else {
+            nums[k] = rightArr[j];
+            j++;
+        }
+        k++;
+    }
+    
+    while (i < n1) {
+        nums[k] = leftArr[i];
+        i++;
+        k++;
+    }
+    
+    while (j < n2) {
+        nums[k] = rightArr[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(vector<int>& nums, int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+        mergeSort(nums, left, mid);
+        mergeSort(nums, mid + 1, right);
+
+        cout<<left<<mid<<right<<" ";
+        merge(nums, left, mid, right);
+    }
+}
     vector<int> sortArray(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        mergeSort(nums, 0, nums.size() - 1);
 
         return nums;
     }
